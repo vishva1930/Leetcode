836ca933699d45b46a63f607cbbd72ff8e0842d6
vishva1930 <guruvishvav97463@gmail.com>
2026-07-04 21:00:30 +0530

    Time: 6 ms (99.46%), Space: 49.7 MB (59.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,19 +1,42 @@
+// import java.util.*;
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+
+//         Map<String, List<String>> map = new HashMap<>();
+
+//         for (String str : strs) {
+
+//             char[] arr = str.toCharArray();
+//             Arrays.sort(arr);
+
+//             String key = new String(arr);
+
+//             map.putIfAbsent(key, new ArrayList<>());
+//             map.get(key).add(str);
+//         }
+
+//         return new ArrayList<>(map.values());
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        Map<String, List<String>> map = new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
 
-        for (String str : strs) {
+        for (String s : strs) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = String.valueOf(ch);
 
-            char[] arr = str.toCharArray();
-            Arrays.sort(arr);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
 
-            String key = new String(arr);
-
-            map.putIfAbsent(key, new ArrayList<>());
-            map.get(key).add(str);
+            map.get(key).add(s);
         }
 
         return new ArrayList<>(map.values());
