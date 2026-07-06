19c81d383ae99971c1d26cdc0a7abca8929e0ddf
vishva1930 <guruvishvav97463@gmail.com>
2026-07-06 21:21:05 +0530

    Time: 1 ms (6.35%), Space: 44.4 MB (26.32%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -8,46 +8,67 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode rotateRight(ListNode head, int k) {
-//         ArrayList<ListNode> list=new ArrayList<>();
-
-
-        
-//     }
-// }
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-          if (head == null || head.next == null || k == 0){
-        return head;
-          }
+        if(head==null || head.next==null || k==0){
+            return head;
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        while(head!=null){
+            list.add(head.val);
+            head=head.next;
+        }
 
-ArrayList<Integer> arr = new ArrayList<>();
+        int n=list.size();
+        k=k%n;
 
-// Store values
-while (head != null) {
-    arr.add(head.val);
-    head = head.next;
-}
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
 
-int n = arr.size();
-k = k % n;
+        for(int i=n-k;i<n;i++){
+            temp.next=new ListNode(list.get(i));
+            temp=temp.next;
+        }
 
-ListNode dummy = new ListNode(0);
-ListNode temp = dummy;
-
-// Add from n-k to n-1
-for (int i = n - k; i < n; i++) {
-    temp.next = new ListNode(arr.get(i));
-    temp = temp.next;
-}
-
-// Add from 0 to n-k-1
-for (int i = 0; i < n - k; i++) {
-    temp.next = new ListNode(arr.get(i));
-    temp = temp.next;
-}
-
-return dummy.next;
+        for(int i=0;i<n-k;i++){
+            temp.next=new ListNode(list.get(i));
+            temp=temp.next;
+        }
+        return dummy.next;   
     }
 }
+// class Solution {
+//     public ListNode rotateRight(ListNode head, int k) {
+//           if (head == null || head.next == null || k == 0){
+//         return head;
+//           }
+
+// ArrayList<Integer> arr = new ArrayList<>();
+
+// // Store values
+// while (head != null) {
+//     arr.add(head.val);
+//     head = head.next;
+// }
+
+// int n = arr.size();
+// k = k % n;
+
+// ListNode dummy = new ListNode(0);
+// ListNode temp = dummy;
+
+// // Add from n-k to n-1
+// for (int i = n - k; i < n; i++) {
+//     temp.next = new ListNode(arr.get(i));
+//     temp = temp.next;
+// }
+
+// // Add from 0 to n-k-1
+// for (int i = 0; i < n - k; i++) {
+//     temp.next = new ListNode(arr.get(i));
+//     temp = temp.next;
+// }
+
+// return dummy.next;
+//     }
+// }
