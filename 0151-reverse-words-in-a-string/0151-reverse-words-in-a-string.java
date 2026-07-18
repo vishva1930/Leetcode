27fa59cd63ffca9f18629e0886408e45df1117b7
vishva1930 <guruvishvav97463@gmail.com>
2026-07-18 12:06:38 +0530

    Time: 13 ms (5.64%), Space: 46.7 MB (16.19%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,65 +1,63 @@
-// class Solution {
-//     public String reverseWords(String s) {
-//         char []arr=s.toCharArray();
-//         String str="";
-//         ArrayList<String> list=new ArrayList<>();
-//         for(int i=0;i<arr.length;i++){
-//             if(arr[i]!=' '){
-//                 str=str+arr[i];
-//             }
-//             else{
-//                 list.add(str);
-//                 str="";
-//             }
-//         }
-//          if (!str.equals("")) {
-//             list.add(str);
-//             str="";
-//         }
-//         String w="";
-//        for(int i=list.size()-1;i>=0;i--){
-//         if(i==0){
-//             w=w+list.get(i);
-//         }
-//         else{
-//             w=w+list.get(i)+" ";
-//         }
-//        }
-//         return w;
-        
-//     }
-// }
-
 class Solution {
     public String reverseWords(String s) {
-        char[] arr = s.toCharArray();
-        String str = "";
-        ArrayList<String> list = new ArrayList<>();
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != ' ') {
-                str += arr[i];
-            } else {
-                if (str.length() > 0) {
-                    list.add(str);
-                    str = "";
-                }
+        char []arr=s.toCharArray();
+        String str="";
+        ArrayList<String> list=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=' '){
+                str+=arr[i];
+            }
+            else{
+                if(str.length()>0)
+                list.add(str);
+                str="";
             }
         }
-
-        if (str.length() > 0) {
+            if (str.length() > 0) {
             list.add(str);
         }
-
-        String w = "";
-
-        for (int i = list.size() - 1; i >= 0; i--) {
-            w += list.get(i);
+        String w="";
+       for(int i=list.size()-1;i>=0;i--){
+        w += list.get(i);
             if (i != 0) {
                 w += " ";
             }
         }
-
-        return w;
+         return w;
+        
     }
-}
+ }
+
+// class Solution {
+//     public String reverseWords(String s) {
+//         char[] arr = s.toCharArray();
+//         String str = "";
+//         ArrayList<String> list = new ArrayList<>();
+
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] != ' ') {
+//                 str += arr[i];
+//             } else {
+//                 if (str.length() > 0) {
+//                     list.add(str);
+//                     str = "";
+//                 }
+//             }
+//         }
+
+//         if (str.length() > 0) {
+//             list.add(str);
+//         }
+
+//         String w = "";
+
+//         for (int i = list.size() - 1; i >= 0; i--) {
+//             w += list.get(i);
+//             if (i != 0) {
+//                 w += " ";
+//             }
+//         }
+
+//         return w;
+//     }
+// }
