c6669fd3c528bcef4c15f2cc5b44ddb748d0e029
vishva1930 <guruvishvav97463@gmail.com>
2026-07-18 12:07:01 +0530

    Time: 12 ms (6.93%), Space: 47.3 MB (5.97%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,63 +1,28 @@
 class Solution {
     public String reverseWords(String s) {
-        char []arr=s.toCharArray();
-        String str="";
-        ArrayList<String> list=new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=' '){
-                str+=arr[i];
-            }
-            else{
-                if(str.length()>0)
-                list.add(str);
-                str="";
+        char[] arr = s.toCharArray();
+        String str = "";
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != ' ') {
+                str += arr[i];
+            } else {
+                if (str.length() > 0)
+                    list.add(str);
+                str = "";
             }
         }
-            if (str.length() > 0) {
+        if (str.length() > 0) {
             list.add(str);
         }
-        String w="";
-       for(int i=list.size()-1;i>=0;i--){
-        w += list.get(i);
+        String w = "";
+        for (int i = list.size() - 1; i >= 0; i--) {
+            w += list.get(i);
             if (i != 0) {
                 w += " ";
             }
         }
-         return w;
-        
+        return w;
+
     }
- }
-
-// class Solution {
-//     public String reverseWords(String s) {
-//         char[] arr = s.toCharArray();
-//         String str = "";
-//         ArrayList<String> list = new ArrayList<>();
-
-//         for (int i = 0; i < arr.length; i++) {
-//             if (arr[i] != ' ') {
-//                 str += arr[i];
-//             } else {
-//                 if (str.length() > 0) {
-//                     list.add(str);
-//                     str = "";
-//                 }
-//             }
-//         }
-
-//         if (str.length() > 0) {
-//             list.add(str);
-//         }
-
-//         String w = "";
-
-//         for (int i = list.size() - 1; i >= 0; i--) {
-//             w += list.get(i);
-//             if (i != 0) {
-//                 w += " ";
-//             }
-//         }
-
-//         return w;
-//     }
-// }
+}
